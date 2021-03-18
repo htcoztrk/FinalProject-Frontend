c5397266758776b475df85c96de7f6c8c9b476f9
htcoztrk <haticeozturk0207@gmail.com>
2021-03-18 16:44:12 +0300

    18.ders

--- northwind/src/app/components/category/category.component.ts
+++ northwind/src/app/components/category/category.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/models/category';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-category',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category.component.css']
 })
 export class CategoryComponent implements OnInit {
-
-  constructor() { }
+  categories:Category[]=[];
+  currentCategory:Category;
+  dataLoaded=false;
+  constructor(private categoyService:CategoryService) { }
 
   ngOnInit(): void {
+    this.getCategories();
   }
-
+getCategories(){
+  this.categoyService.getCategories().subscribe(response=>{
+    this.categories=response.data;
+    this.dataLoaded=true;
+  })
+}
+setCurrentCategory(category:Category){
+//  console.log(category.categoryName)
+this.currentCategory=category;
+}
+getCurrentCategoryClass(category:Category){
+   if(category==this.currentCategory){
+     return "list-group-item active"
+   }
+   else{
+     return "list-group-item"
+   }
+}
 }
